1ef4e6d330eaf201777bef973a5e841c0b662dee
pkanch838_comcast <PavanKumar_kanchanam@comcast.com>
2026-07-16 12:38:25 +0530

    implemented enhancements to main.py file

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from datetime import datetime
 
-import requests as rq
+import requests
+url="https://jsonplaceholder.typicode.com/users"
+print(f"Calling API..\n"
+      f"Method:GET\n"
+f"URL:{url}")
 
-print("Application started")
-response= rq.get("https://jsonplaceholder.typicode.com/users")
+response= requests.get(url)
 
 
 assert response.status_code == 200,"API failed"
+print("API Executed successfully and status code is 200")
 tot_time= response.elapsed.total_seconds()
-print("Total time: ", tot_time)
+print(f"Total_response_time= {tot_time:.3f}")
+print("\n" + "* " * 30)
 for key,value in response.headers.items():
     print(f"{key}: {value}")
+print("\n" + "* " * 30)
 
-print(response.status_code)
-print(response.json())
+for user in response.json():
+    print(user)
 
 print("Application completed")
 
